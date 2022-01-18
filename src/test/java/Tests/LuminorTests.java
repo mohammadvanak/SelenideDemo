e7f5322350515bcad77fad873436498c55cc8360
002NAR744 <002NAR744@WIN-S8HJSFNT25U>
2022-01-18 14:50:30 +0530

    Some changes

--- src/test/java/Tests/LuminorTests.java
+++ src/test/java/Tests/LuminorTests.java
@@ -6,24 +6,31 @@ import org.testng.annotations.Test;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.ElementsCollection;
 
+import Utils.ExcelUtil;
+
 import static com.codeborne.selenide.Selenide.*;
+
+import java.io.IOException;
+import java.util.List;
+
 import static com.codeborne.selenide.Condition.*;
 
 public class LuminorTests {
 	
 	@Test
-	public void currencyRates() throws InterruptedException
+	public void currencyRates() throws InterruptedException, IOException
 	{
+		Configuration.reportsFolder = "test-output/Screenshots";
 		//Configuration.pageLoadStrategy="eager";
+		List<String> data=ExcelUtil.readExcel(".//Data", "TestData.xlsx", "CurrencyRates");
 		open("https://luminor.ee/currency-rates");
 		$(By.className("loading-app")).should(disappear);
-		Thread.sleep(20000);
+		Thread.sleep(20000);// This is being used because the site is very slow and selenide is giving exceptions very early.
 			$(By.cssSelector(".bp3-popover-wrapper.custom-control.currency-select.currency-select-large")).shouldBe(visible);
 		  $(By.cssSelector(".bp3-popover-wrapper.custom-control.currency-select.currency-select-large")).click();
 		  $(By.xpath(" //button[contains(text(),\"USD\")]")).click();
-		  $(By.className("control-input-input")).setValue("100");
-		  //button[contains(text(),"USD")]
-		  Thread.sleep(10000);
+		  $(By.className("control-input-input")).setValue(data.get(0));
+		  Thread.sleep(5000);
 		  ElementsCollection inputFields= $$(By.className("control-input-input"));
 		  String iBuyTextvalue=inputFields.get(2).getAttribute("value");
 		  System.out.println(iBuyTextvalue);
---
+++ src/test/java/Utils/ExcelUtil.java
@@ -0,0 +1,48 @@
+package Utils;
+
+import java.io.*;
+import java.util.List;
+
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.sl.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.DataFormatter;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class ExcelUtil {
+	public static List<String> readExcel(String filePath,String fileName,String sheetName) throws IOException{
+
+
+	    File file = new File(filePath+"\\"+fileName);
+
+	    FileInputStream inputStream = new FileInputStream(file);
+
+	    XSSFWorkbook Workbook = null;
+
+	    String fileExtensionName = fileName.substring(fileName.indexOf("."));
+	    if(fileExtensionName.equals(".xlsx")){
+
+	    	Workbook = new XSSFWorkbook(inputStream);
+
+	    }
+	    
+	    XSSFSheet Sheet = Workbook.getSheet(sheetName);
+
+	    int rowCount = Sheet.getLastRowNum()-Sheet.getFirstRowNum();
+	    List<String> data=null;
+	    DataFormatter formatter = new DataFormatter();
+	    for (int i = 1; i < rowCount+1; i++) {
+
+	        Row row = Sheet.getRow(i);
+
+	        for (int j = 0; j < row.getLastCellNum(); j++) {
+	        	data.add(formatter.formatCellValue(row.getCell(j)));
+	            //System.out.print(row.getCell(j).getStringCellValue()+"|| ");
+
+	        }
+	    } 
+	    return data;
+	    }
+	}
